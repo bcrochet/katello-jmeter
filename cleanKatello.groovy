10dffd01b0599749bb3cd762ca2ac797d4fcdc34
Brad P. Crochet <brad@redhat.com>
2012-03-19 13:11:47 -0400

    Add cleanup code

--- cleanKatello.groovy
+++ cleanKatello.groovy
@@ -2,23 +2,51 @@
 import groovyx.net.http.*
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
+import java.security.KeyStore
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.ssl.SSLSocketFactory
 
-def katello = new RESTClient( 'https://localhost/katello/api/' )
-katello.auth.basic 'username', 'password'
+// Temp import til thin server fixed
+import groovy.json.JsonSlurper
 
-// perform a GET request, expecting JSON response data
-try {
-    println "Get consumers"
-    def resp = katello.get( path: 'consumers' )
-    println "Consumers:"
-    println resp.data
-    resp.data.each { consumer ->
-        def uuid = consumer.uuid
-        println "Deleting uuid: " + uuid
-        def delresp = katello.delete( path: 'consumers/' + uuid )
-    }
-} catch ( ex ) { 
-    println ex.response.status
-    println ex.response
+def output = """curl -k -N --user admin:admin https://bcrochet-katello.usersys.redhat.com/katello/api/consumers""".execute().text
+def consumers = new JsonSlurper().parseText(output)
+consumers.each { consumer ->
+    def uuid = consumer.uuid
+    println "Delete uuid: ${uuid}"
+    println """curl -k -X DELETE -N --user admin:admin https://bcrochet-katello.usersys.redhat.com/katello/api/consumers/${uuid}""".execute().text
 }
+
+
+
+
+
+//def keystore = KeyStore.getInstance( KeyStore.defaultType )
+//getClass().getResource( "truststore.jks" ).withInputStream {
+//    keystore.load( it, "password".toCharArray() )
+//}
+
+//def katello = new RESTClient( 'https://bcrochet-katello.usersys.redhat.com/katello/api/' )
+//katello.client.connectionManager.schemeRegistry.register( new Scheme("https", new SSLSocketFactory(keystore), 443) )
+//katello.auth.basic 'admin', 'admin'
+
+// When rubygem-thin gets the auth fixed, we can re-enable this
+// 
+// perform a GET request, expecting JSON response data
+//try {
+//    println "Get consumers"
+//    def resp = katello.get( path: 'consumers' )
+//    println "Consumers:"
+//    println resp.data
+//    resp.data.each { consumer ->
+//        def uuid = consumer.uuid
+//        println "Deleting uuid: " + uuid
+//        def delresp = katello.delete( path: 'consumers/' + uuid )
+//    }
+//} catch ( ex ) { 
+//    println "response status: ${ex.response.statusLine}"
+//    ex.response.headers.each { h ->
+//        println " ${h.name} : ${h.value}"
+//    }
+//}
 
